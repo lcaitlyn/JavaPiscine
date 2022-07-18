baf8e816789989b79c38b072c7b7f5e0295a656a
Lachelle Caitlyn <lcaitlyn@un-b6.kzn.21-school.ru>
2022-07-18 17:23:04 +0300

    исправил ex00

--- day00/ex00/Program.java
+++ day00/ex00/Program.java
@@ -3,11 +3,29 @@ public class Program {
     public static int getSum(int x) {
         int sum = 0;
 
-        while (x > 0) {
-            sum += x % 10;
-            x /= 10;
-        }
-        return sum;
+        sum += x / 100000;
+
+        x %= 100000;
+
+        sum += x / 10000;
+
+        x %= 10000;
+
+        sum += x / 1000;
+
+        x %= 1000;
+
+        sum += x / 100;
+
+        x %= 100;
+
+        sum += x / 10;
+
+        x %= 10;
+
+        sum += x;
+
+        return sum;        
     }
 
     public static void main(String[] args) {
