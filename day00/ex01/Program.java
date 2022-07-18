9ec1ca1029f5a6a71ac8428932067b7e96768064
lcaitlyn <92291298+lcaitlyn@users.noreply.github.com>
2022-07-18 22:58:51 +0300

    обновил ex01. убрал класс Math

--- day00/ex01/Program.java
+++ day00/ex01/Program.java
@@ -2,8 +2,33 @@ import java.util.Scanner;
 
 public class Program {
 
+    public static double mySqrt(int x) {
+        double flag = 0.1d;
+        
+        double val = x;
+        
+        double last;
+        
+        if (x <= 0) {
+            return 0;
+        }
+        
+        do {
+            last = val;
+            val = (val + x / val) / 2;
+        } while (val - last > flag || val - last < -flag);
+        
+        return  val;
+    }
+    
+    public static int myCeil(double x) {
+        if (x - (int)x > 0)
+            return (int)x + 1;
+        return (int)x;
+    }
+    
     public static boolean isPrime(int x) {
-        int root = ((int) Math.ceil(Math.sqrt(x)));
+        int root = myCeil(mySqrt(x));
 
         int i = 2;
 
