a366d65630fa531d3b219da242a5d56000da1d3e
Lachelle Caitlyn <lcaitlyn@un-r4.kzn.21-school.ru>
2022-09-06 17:21:30 +0300

    started day05

---
+++ day05/schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS mytable (
+    column DataType TableConstraint DEFAULT default_value,
+    another_column DataType TableConstraint DEFAULT default_value
+);
---
+++ day05/src/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS mytable (
+    column DataType TableConstraint DEFAULT default_value,
+    another_column DataType TableConstraint DEFAULT default_value,
+    …
+);
