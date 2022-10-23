a29b0a807574091627258391f6e81d5d64494bf2
pc <mrrifatcool@gmail.com>
2022-10-23 17:03:23 +0300

    added day05 ex02

---
+++ day05/ex02/Chat/src/main/java/edu/school21/chat/app/Program.java
@@ -0,0 +1,38 @@
+package main.java.edu.school21.chat.app;
+
+import com.zaxxer.hikari.HikariDataSource;
+import main.java.edu.school21.chat.models.Chatroom;
+import main.java.edu.school21.chat.models.Message;
+import main.java.edu.school21.chat.models.User;
+import main.java.edu.school21.chat.repositories.MessagesRepositoryJdbcImpl;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Scanner;
+
+public class Program {
+
+    public static DataSource makeHikariDataSource() {
+        HikariDataSource hikariDataSource = new HikariDataSource();
+
+        hikariDataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/postgres");
+        hikariDataSource.setUsername("postgres");
+        hikariDataSource.setPassword("");
+
+        return hikariDataSource;
+    }
+
+    public static void main(String[] args) {
+        User admin = new User(1, "Admin", "Admin");
+        Chatroom adminChatRoom = new Chatroom(1, "AdminChatRoom", admin);
+        Message message = new Message(-1, admin, adminChatRoom, "Я тут админ!!!", LocalDateTime.now());
+
+        MessagesRepositoryJdbcImpl messagesRepositoryJdbc = new MessagesRepositoryJdbcImpl(makeHikariDataSource());
+
+        messagesRepositoryJdbc.save(message);
+
+        System.out.println("ID сообщения в БД = " + message.getId());
+    }
+}
---
+++ day05/ex02/Chat/src/main/java/edu/school21/chat/repositories/MessagesRepository.java
@@ -0,0 +1,12 @@
+package main.java.edu.school21.chat.repositories;
+
+import main.java.edu.school21.chat.exception.NotSavedSubEntityException;
+import main.java.edu.school21.chat.models.Message;
+
+import java.sql.SQLException;
+import java.util.Optional;
+
+public interface MessagesRepository {
+    Optional<Message> findById(Long id) throws SQLException;
+    boolean save(Message message) throws NotSavedSubEntityException;
+}
